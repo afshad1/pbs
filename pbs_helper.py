9dda9fe76e7e2d3351627bdaf75150a99db33844
afshad <afshad.syed@gmail.com>
2020-03-02 21:48:50 +0100

    added check and sum only int numbers in values

--- pbs_helper.py
+++ pbs_helper.py
@@ -1,5 +1,10 @@
 def calcSum(pbs):
+# Calculating sums for each entries in pbs. Check included for int numbers.
     sum = 0
     for p in pbs:
-        sum += int(p['value'])
+        try:
+            pVal = int(p['value'])
+            sum += pVal
+        except ValueError:
+            pass        
     return sum
